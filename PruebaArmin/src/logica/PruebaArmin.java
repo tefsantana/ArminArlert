4c9d3079ef79273f5adce1231930d191cb974c0b
Estefanía Santana <83146564+tefsantana@users.noreply.github.com>
2021-09-14 23:23:21 -0300

    Add files via upload

---
+++ PruebaArmin/src/logica/PruebaArmin.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import igu.Principal;
+
+/**
+ *
+ * @author Seba
+ */
+public class PruebaArmin {
+    public static void main(String[] args) {
+        
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+}
+
